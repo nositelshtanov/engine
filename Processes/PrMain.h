f6f498ea018ca64e67c53284ebe41dd95c848408
nositelshtanov <groshev523000@gmail.com>
2025-06-15 21:05:22 +0300

    some update

--- Processes/PrMain.h
+++ Processes/PrMain.h
@@ -5,7 +5,45 @@
 class PrMain : public ProcessBase
 {
 public:
+    using BaseClass = ProcessBase;
+
     PrMain()
        : ProcessBase(static_cast<PrId>(PrIds::Main), nullptr) 
     {}
+
+    virtual bool Run() override
+    { 
+        bool res = BaseClass::Run();        
+        CreateChildProcesses();
+        return res; 
+    }
+
+    virtual bool Stop() override
+    { 
+        bool res = BaseClass::Stop();
+        return res;
+    }
+
+    virtual bool ReceiveEvent(const Event& event) override
+    {
+        auto et = event.GetEventType();
+        switch (et)
+        {
+        case EventType::MouseEvent:
+            /* code */
+            break;
+        case EventType::KeyBoardEvent:
+            /* code */
+            break;
+        default:
+            return false;
+            break;
+        }
+    }
+
+private:
+    void CreateChildProcesses()
+    {
+        
+    }
 };
