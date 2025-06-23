f05df4337f34c6bec2608802fb2457f2f4adaacc
nositelshtanov <groshev523000@gmail.com>
2025-06-23 22:25:40 +0300

    add debug info

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -o app main.cpp EventBus/EventBus.cpp Processes/ProcessCreator.cpp Scene/Object3DBase.cpp Scene/Scene.cpp Scene/ObjectsConnector.cpp -lsfml-system -lsfml-window -lsfml-graphics -lGL -lGLEW
+g++ -g -o app main.cpp EventBus/EventBus.cpp Processes/ProcessCreator.cpp Scene/Object3DBase.cpp Scene/Scene.cpp Scene/ObjectsConnector.cpp -lsfml-system -lsfml-window -lsfml-graphics -lGL -lGLEW
